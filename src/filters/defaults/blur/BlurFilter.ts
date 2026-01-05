1f3134d07c94b853b47db8f3d5947dc428e8bc3d
Master-Hash <hash@hash.moe>
2026-01-05 23:42:36 +0800

    part 8

--- src/filters/defaults/blur/BlurFilter.ts
+++ src/filters/defaults/blur/BlurFilter.ts
@@ -161,7 +161,7 @@ export class BlurFilter extends Filter
      */
     public blurYFilter: BlurFilterPass;
 
-    private _repeatEdgePixels = false;
+    #repeatEdgePixels = false;
 
     /**
      * @param {filters.BlurFilterOptions} options - The options of the blur filter.
@@ -250,7 +250,7 @@ export class BlurFilter extends Filter
 
     protected updatePadding(): void
     {
-        if (this._repeatEdgePixels)
+        if (this.#repeatEdgePixels)
         {
             this.padding = 0;
         }
@@ -443,12 +443,12 @@ export class BlurFilter extends Filter
      */
     get repeatEdgePixels(): boolean
     {
-        return this._repeatEdgePixels;
+        return this.#repeatEdgePixels;
     }
 
     set repeatEdgePixels(value: boolean)
     {
-        this._repeatEdgePixels = value;
+        this.#repeatEdgePixels = value;
         this.updatePadding();
     }
 }
